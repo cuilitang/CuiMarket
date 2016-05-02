15d73674664ba608c2fb978c70b51fc0c87f5daf
cuilitang <cuilitang@126.com>
2016-05-02 12:38:39 +0800

    滚动TextView，添加ValueAnimator,使整体布局ScrollView滚动至TextView尾部

--- src/cui/litang/cuimarket/holder/DetailDesHolder.java
+++ src/cui/litang/cuimarket/holder/DetailDesHolder.java
@@ -1,9 +1,18 @@
 package cui.litang.cuimarket.holder;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.View.MeasureSpec;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import cui.litang.cuimarket.R;
 import cui.litang.cuimarket.bean.AppInfo;
@@ -16,6 +25,8 @@ public class DetailDesHolder extends BaseHolder<AppInfo> implements OnClickListe
 	private ImageView des_arrow;
 	private AppInfo appInfo;
 	private RelativeLayout des_layout;
+	private boolean mIsInit = true;
+	private ScrollView mScrollView;
 
 	@Override
 	public View initView() {
@@ -25,7 +36,7 @@ public class DetailDesHolder extends BaseHolder<AppInfo> implements OnClickListe
 		des_content = (TextView)view.findViewById(R.id.des_content); //简介内容
 		des_author = (TextView)view.findViewById(R.id.des_author); //作者
 		des_arrow = (ImageView)view.findViewById(R.id.des_arrow);//收放箭头
-		
+		des_arrow.setTag(false);
 		des_layout.setOnClickListener(this);
 		return view;
 	}
@@ -37,8 +48,29 @@ public class DetailDesHolder extends BaseHolder<AppInfo> implements OnClickListe
 		des_content.setText(appInfo.getDes());
 		des_author.setText("作者:"+appInfo.getAuthor());
 		
-		des_content.setMaxLines(5); //默认收起
-		des_arrow.setTag(true);
+		if(mIsInit){
+			des_content.getLayoutParams().height = measureShortHeight();
+			mIsInit = false;
+		}
+	}
+
+	private int measureShortHeight() {
+
+		int measuredWidth = des_content.getMeasuredWidth();
+		int width = MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY);
+		int height = MeasureSpec.makeMeasureSpec(2000, MeasureSpec.AT_MOST);
+		TextView textView = getTextView();
+		textView.setLines(7);
+		textView.setMaxLines(7);
+		textView.measure(width, height);
+		return textView.getMeasuredHeight();
+	}
+
+	private TextView getTextView() {
+		TextView textView = new TextView(UIUtils.getContext());
+		textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,14);
+		textView.setText(getData().getDes());
+		return textView;
 	}
 
 	/**
@@ -58,16 +90,94 @@ public class DetailDesHolder extends BaseHolder<AppInfo> implements OnClickListe
 	}
 
 	private void expend() {
-		boolean flag = (Boolean) des_arrow.getTag();
 		
+		final LayoutParams layoutParams = des_content.getLayoutParams();
+		int shortHeight = measureShortHeight();
+		int longHeight = measureLongHeight();
+		ValueAnimator va;
+		final boolean flag = (Boolean) des_arrow.getTag();
 		if(flag){
 			des_arrow.setTag(false);
-			des_content.setMaxLines(100); //打开
-			des_arrow.setImageResource(R.drawable.arrow_up);
+			va = ValueAnimator.ofInt(longHeight,shortHeight);
 		}else{
 			des_arrow.setTag(true);
-			des_content.setMaxLines(5); //关闭
-			des_arrow.setImageResource(R.drawable.arrow_down);
+			va = ValueAnimator.ofInt(shortHeight,longHeight);
 		}
+		
+		des_layout.setEnabled(false);  //动画进行中，停止时间反应
+		va.addUpdateListener(new AnimatorUpdateListener() {
+
+			@Override
+			public void onAnimationUpdate(ValueAnimator animation) {
+
+				layoutParams.height = (Integer) animation.getAnimatedValue();
+				des_content.setLayoutParams(layoutParams);
+				if(!flag){
+					if(mScrollView == null){
+						mScrollView = getSrollView();
+					}else{
+						mScrollView.scrollTo(0,mScrollView.getHeight());
+					}
+					
+				}
+				
+			}
+
+		} );
+		
+		va.addListener(new AnimatorListener() {
+			
+			@Override
+			public void onAnimationStart(Animator animation) {}
+				
+			@Override
+			public void onAnimationRepeat(Animator animation) {}
+			
+			@Override
+			public void onAnimationEnd(Animator animation) {
+
+				boolean flag = (Boolean) des_arrow.getTag();
+				des_arrow.setImageResource(flag?R.drawable.arrow_up:R.drawable.arrow_down);
+				des_layout.setEnabled(true);
+			}
+			
+			@Override
+			public void onAnimationCancel(Animator animation) {}
+			
+		});
+		
+		va.setDuration(300);
+		va.start();
+		
+		
+	}
+
+	private int measureLongHeight() {
+
+		int measuredWidth = des_content.getMeasuredWidth();
+		int width = MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY);
+		int height = MeasureSpec.makeMeasureSpec(2000, MeasureSpec.AT_MOST);
+		TextView textView = getTextView();
+		textView.measure(width, height);
+		return textView.getMeasuredHeight();
+	}
+	
+	private ScrollView getSrollView() {
+
+		ScrollView scv = null;
+		View currentView = des_layout;
+		while(true){
+			ViewParent parent = currentView.getParent();
+			
+			if(parent == null ||!(parent instanceof View)){
+				break;
+			}else if(parent instanceof ScrollView){
+				scv = (ScrollView) parent;
+				break;
+			}else{
+				currentView = (View) parent;
+			}
+		}
+		return scv;
 	}
 }
